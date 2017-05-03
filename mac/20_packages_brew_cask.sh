eaa285b5975d71959200bd3ebdd296c6a7686090
Max Lobur <max_lobur@outlook.com>
2017-05-03 16:13:30 +0300

    Revise brew cask

--- mac/20_packages_brew_cask.sh
+++ mac/20_packages_brew_cask.sh
@@ -14,9 +14,9 @@ brew cask install docker
 brew cask install dropbox
 brew cask install evernote
 brew cask install firefox
-brew cask install flux
-brew cask install gitup
-brew cask install hipchat
+#brew cask install flux  # screen temp based on time
+brew cask install gitup 
+#brew cask install hipchat  # atlassian slack
 brew cask install iterm2
 brew cask install minikube
 brew cask install ngrok
@@ -29,11 +29,13 @@ brew cask install slack
 brew cask install spotify
 brew cask install the-unarchiver
 brew cask install telegram
+brew cask install torbrowser
 brew cask install tunnelbear
 brew cask install virtualbox
 brew cask install virtualbox-extension-pack
 brew cask install vlc
 brew cask install yujitach-menumeters
+brew cask install zoomus
 
 #cask install sidestep # Securing public wifi
 #cask install transmit # FTP client

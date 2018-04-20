342b986022c9a9035d55a84842dfdc3819ca1eb7
Max Lobur <max_lobur@outlook.com>
2018-04-20 17:50:30 +0300

    kops

--- mac/10_packages_brew.sh
+++ mac/10_packages_brew.sh
@@ -28,6 +28,7 @@ brew install hub
 brew install kubernetes-cli
 brew install kubernetes-helm
 brew install kubectx --with-short-names
+brew install kops
 brew install mc
 brew install nmap
 brew install openssl

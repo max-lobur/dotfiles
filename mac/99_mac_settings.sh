7646e2fb9e063cd90ba8d7f776bfcaf7185bcfdc
Max Lobur <max_lobur@outlook.com>
2018-05-02 14:58:59 +0300

    More mac settings

--- mac/99_mac_settings.sh
+++ mac/99_mac_settings.sh
@@ -2,29 +2,47 @@
 
 echo " -------------------- Applying macOS settings... -----------------------"
 
-#Faster Expose
-defaults write com.apple.dock expose-animation-duration -float 0.20;
-#Set a blazingly fast keyboard repeat rate
+# Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
-#Set a shorter Delay until key repeat
+
+# Set a shorter Delay until key repeat
 defaults write NSGlobalDomain InitialKeyRepeat -int 12
-#Show the ~/Library folder
+
+# Show the ~/Library folder
 chflags nohidden ~/Library
+
+# Show the /Volumes folder
+sudo chflags nohidden /Volumes
+
 # Show hidden files
-#defaults write com.apple.finder AppleShowAllFiles -bool true
+# defaults write com.apple.finder AppleShowAllFiles -bool true
+
 # Empty Trash securely by default
 defaults write com.apple.finder EmptyTrashSecurely -bool true
+
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
 # Disable the "Are you sure you want to open this application?" dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
+
 # Show all filename extensions in Finder
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
 # Dock
 defaults write com.apple.dock magnification -boolean false
-defaults write com.apple.dock tilesize -integer 40
+defaults write com.apple.dock tilesize -integer 50
+defaults write com.apple.dock minimize-to-application -bool true
+defaults write com.apple.dock expose-animation-duration -float 0.1;
+killall Dock # restart
+
+# Install Command Line Tools for Xcode
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+sudo softwareupdate -ai
+rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
 #Store screenshots in subfolder on desktop
 # mkdir ~/Desktop/Screenshots

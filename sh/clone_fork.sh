67a367f7531fe132d7bf0cc29f8f757a7396a6ad
Max Lobur <max_lobur@outlook.com>
2018-09-18 21:16:33 +0300

    clone fork script

---
+++ sh/clone_fork.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+usage()
+{
+    echo "Usage:"
+    echo `basename $0`" <repo_url>"
+    echo "Example:"
+    echo `basename $0`" git@github.com:max-lobur/dotfiles.git"
+    exit 1
+}
+[[ "$#" -ne 1 ]] && usage;
+URL=$1
+NAME=`basename ${URL} .git`
+
+git clone ${URL}
+cd ${NAME}
+hub fork ${URL} || true
+git remote rename origin upstream
+git remote add origin "git@github.com:max-lobur/${NAME}.git"
+echo "Git remotes:"
+git remote -v
+echo "DONE. cd ${NAME}"

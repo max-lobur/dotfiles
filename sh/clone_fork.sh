728f75a8decbdef67f12f2588b3ae3852268862a
Max Lobur <max_lobur@outlook.com>
2018-10-10 19:28:10 +0300

    Extract GH_USERNAME

--- sh/clone_fork.sh
+++ sh/clone_fork.sh
@@ -2,9 +2,10 @@
 usage()
 {
     echo "Usage:"
-    echo `basename $0`" <repo_url>"
+    echo "  "`basename $0`" <repo_url>"
     echo "Example:"
-    echo `basename $0`" git@github.com:max-lobur/dotfiles.git"
+    echo "  "`basename $0`" git@github.com:${GH_USERNAME}/dotfiles.git"
+    echo "Also make sure your GH_USERNAME env var is set."
     exit 1
 }
 [[ "$#" -ne 1 ]] && usage;
@@ -15,7 +16,7 @@ git clone ${URL}
 cd ${NAME}
 hub fork --no-remote ${URL} || true
 git remote rename origin upstream
-git remote add origin "git@github.com:max-lobur/${NAME}.git"
+git remote add origin "git@github.com:${GH_USERNAME}/${NAME}.git"
 echo "Git remotes:"
 git remote -v
 echo "DONE. cd ${NAME}"

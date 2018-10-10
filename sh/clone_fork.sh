f7a5ec9c3146872541d8d7d8b579a8d4b534fc8a
Max Lobur <max_lobur@outlook.com>
2018-10-10 19:31:46 +0300

    take GH_USERNAME from hub config

--- sh/clone_fork.sh
+++ sh/clone_fork.sh
@@ -4,13 +4,13 @@ usage()
     echo "Usage:"
     echo "  "`basename $0`" <repo_url>"
     echo "Example:"
-    echo "  "`basename $0`" git@github.com:${GH_USERNAME}/dotfiles.git"
-    echo "Also make sure your GH_USERNAME env var is set."
+    echo "  "`basename $0`" git@github.com:max-lobur/dotfiles.git"
     exit 1
 }
 [[ "$#" -ne 1 ]] && usage;
 URL=$1
 NAME=`basename ${URL} .git`
+GH_USERNAME=`cat ~/.config/hub | grep user | cut -d":" -f2 | xargs`
 
 git clone ${URL}
 cd ${NAME}

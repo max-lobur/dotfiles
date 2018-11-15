6d2703c5e93afa169d4fd3cdc030bf7cb8005997
Max Lobur <max_lobur@outlook.com>
2018-11-15 14:05:50 +0200

    kubefwd

--- mac/10_packages_brew.sh
+++ mac/10_packages_brew.sh
@@ -34,6 +34,7 @@ brew install kubernetes-cli
 brew install kubernetes-helm
 brew install kubectx --with-short-names
 brew install kube-ps1
+brew install txn2/tap/kubefwd
 brew install kops
 brew install mc
 brew install nmap

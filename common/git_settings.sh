0c2ecc4ceaac4610ce12d54292c34a2fb1d34c47
Max Lobur <max_lobur@outlook.com>
2019-04-04 15:09:56 +0300

    merge branchdate

--- common/git_settings.sh
+++ common/git_settings.sh
@@ -12,8 +12,7 @@ git config --global push.default simple
 git config --global gitreview.scheme=ssh
 git config --global gitreview.username=max_lobur
 
-git config --global alias.br "for-each-ref --sort='authordate' --format='%(refname)%09%(authordate)' refs/heads | tail "
-git config --global alias.branchdate "\!git for-each-ref --sort='authordate' --format='%(refname)%09%(authordate)' refs/heads | sed -e 's-refs/heads/--' | tail"
+git config --global alias.br "\!git for-each-ref --sort='authordate' --format='%(refname)%09%(authordate)' refs/heads | sed -e 's-refs/heads/--' | tail"
 
 
 ### MAC

873fb6ff9579c8809ee18aa07f664391601f13a8
Max Lobur <max_lobur@outlook.com>
2019-11-27 15:46:03 -0800

    grpcurl

--- mac/10_packages_brew.sh
+++ mac/10_packages_brew.sh
@@ -19,6 +19,7 @@ brew install daemon
 brew install direnv
 brew install flatbuffers
 brew install git
+brew install grpcurl
 brew install --force gettext # envsubst
 brew link --force gettext
 brew install gnu-tar

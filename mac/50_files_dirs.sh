5ee8ab57a6bbb64ac4a637d7c584ff46897d48a9
Max Lobur <max.lobur@verygood.systems>
2021-01-14 13:06:33 +0200

    Remote unused dirs

--- mac/50_files_dirs.sh
+++ mac/50_files_dirs.sh
@@ -7,8 +7,6 @@ ln -sf $HOME/Dropbox/settings/dot_ssh/config $HOME/.ssh/config
 touch $HOME/.ssh/agent_env
 
 # Dirs
-mkdir -p $HOME/venvs
 mkdir -p $HOME/git
-mkdir -p $HOME/Install
 mkdir -p $HOME/go
 

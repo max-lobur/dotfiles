439853588c1f9f6b3e4f76bf639e298f343c668c
Max Lobur <max_lobur@outlook.com>
2022-10-24 21:46:02 +0300

    Dropbox upgrade

--- init/mac/50_files_dirs.sh
+++ init/mac/50_files_dirs.sh
@@ -3,7 +3,7 @@
 # SSH
 mkdir -p $HOME/.ssh
 mkdir -p $HOME/.ssh/conf.d/
-ln -sf $HOME/Dropbox/settings/dot_ssh/config $HOME/.ssh/config
+ln -sf $HOME/Library/CloudStorage/Dropbox/settings/dot_ssh/config $HOME/.ssh/config
 touch $HOME/.ssh/agent_env
 
 # Dirs

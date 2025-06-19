6efe38cf34555f9c58c0b602512c0dec81f03ec1
Max Lobur <max_lobur@outlook.com>
2025-06-19 23:28:32 +0200

    updates

--- init/mac/90_vscode.sh
+++ init/mac/90_vscode.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 # Settings
-conf_dir="${HOME}/Library/Application Support/Code/User/"
-ln -sf ~/git/dotfiles/settings/vscode/settings.json "${conf_dir}/settings.json"
-ln -sf ~/git/dotfiles/settings/vscode/keybindings.json "${conf_dir}/keybindings.json"
+conf_dir="${HOME}/Library/Application Support/Cursor/User/"
+ln -sf ~/git/dotfiles/settings/cursor/settings.json "${conf_dir}/settings.json"
+ln -sf ~/git/dotfiles/settings/cursor/keybindings.json "${conf_dir}/keybindings.json"
 echo "> ll ${conf_dir}"
 ls -la "${conf_dir}"
 
 # Extensions
-# cursor --list-extensions > settings/vscode/extensions.txt
-extensions="${HOME}/git/dotfiles/settings/vscode/extensions.txt"
+# cursor --list-extensions > settings/cursor/extensions.txt
+extensions="${HOME}/git/dotfiles/settings/cursor/extensions.txt"
 while IFS= read -r ext
 do
     echo " ==> Installing: ${ext}"
     cursor --install-extension ${ext}
 done < "${extensions}"
-echo " === VSCode Extensions: === "
+echo " === Cursor Extensions: === "
 cursor --list-extensions

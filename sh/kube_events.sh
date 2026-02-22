281e7b8b9374ec3d5714938215839078000944f8
Max Lobur <max_lobur@outlook.com>
2026-02-22 22:57:06 +0100

    Updates

--- sh/kube_events.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-kubectl get ev --all-namespaces --sort-by=lastTimestamp | grep -e Warning -e Error
--- sh/kube_watch.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-watch -n5 "kubectl get no && echo && kubectl get po --all-namespaces | grep -i -e creating -e pending -e crash -e backoff -e fail -e error -e initializing"
